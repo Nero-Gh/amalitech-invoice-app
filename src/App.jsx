8945bd9b2de9c48e61a1bf69528bbf140e78c3e7
nero-gh <neroaboagye@gmail.com>
2023-04-08 23:44:05 +0100

    Invoice App Starter Files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="#" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Invoice App</h1>
+    </div>
+  );
+}
+
+export default App;
